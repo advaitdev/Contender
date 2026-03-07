2dec98114af4d2c1bf60cb334460db5ef4840888
advait <git@advait.fun>
2026-03-07 00:42:08 -0500

    Added minimessage in chat

--- src/main/java/me/advait/contender/listener/ChatListener.java
+++ src/main/java/me/advait/contender/listener/ChatListener.java
@@ -3,6 +3,7 @@ package me.advait.contender.listener;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import me.advait.contender.util.StringUtil;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -10,13 +11,16 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onChat(AsyncChatEvent event) {
-        event.renderer((source, sourceDisplayName, message, viewer) ->
-                Component.empty()
-                        .append(StringUtil.getPlayerHead(source))
-                        .append(Component.space())
-                        .append(sourceDisplayName) // respects nick/plugins
-                        .append(Component.text(": "))
-                        .append(message)
-        );
+        event.renderer((source, sourceDisplayName, message, viewer) -> {
+            Component parsedMessage = MiniMessage.miniMessage().deserialize(event.signedMessage().message());
+
+            return Component.empty()
+                    .append(StringUtil.getPlayerHead(source))
+                    .append(Component.space())
+                    .append(sourceDisplayName)
+                    .append(Component.text(": "))
+                    .append(parsedMessage);
+        });
     }
+
 }
